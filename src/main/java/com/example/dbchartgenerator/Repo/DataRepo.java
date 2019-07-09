9284dca9809002abe247318093fab94d3debb932
daeunnpark <daeunn@gmail.com>
2019-07-09 17:36:02 -0400

    react added as src/main/js

--- src/main/java/com/example/dbchartgenerator/Repo/DataRepo.java
+++ src/main/java/com/example/dbchartgenerator/Repo/DataRepo.java
@@ -10,3 +10,33 @@ import com.example.dbchartgenerator.Model.Data;
 public interface DataRepo extends CrudRepository<Data, Long> {
 
 }
+
+
+
+/*
+
+@Component
+public class WebsiteRepository {
+    private static final Map<Integer, Website> WEBSITES = new HashMap<>();
+    private static Integer counter = 1;
+    public List<Website> findAll() {
+        return new ArrayList<>(WEBSITES.values());
+    }
+    public Website findById(Integer id) {
+        return WEBSITES.get(id);
+    }
+    public void add(Website website) {
+        counter++;
+        website.setId(counter);
+        WEBSITES.put(counter, website);
+    }
+    public void update(Website website) {
+        WEBSITES.put(website.getId(), website);
+    }
+    public void delete(Integer id) {
+        WEBSITES.remove(id);
+    }
+}
+
+
+ */
