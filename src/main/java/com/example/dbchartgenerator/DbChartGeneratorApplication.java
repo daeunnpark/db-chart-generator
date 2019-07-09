08f925941fca884650818e5b609f8916428bfa3a
Daeun Park <daeunpark@localhost.localdomain>
2019-07-09 10:17:31 -0400

    spring boot initialized

---
+++ src/main/java/com/example/dbchartgenerator/DbChartGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbchartgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbChartGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbChartGeneratorApplication.class, args);
+	}
+
+}
