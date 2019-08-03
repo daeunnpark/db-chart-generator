0aff77625f477db6030bba500225a7367875b0f5
daeunnpark <daeunn@gmail.com>
2019-08-03 11:35:52 -0400

    file structure updated

---
+++ server/springbootJava/src/main/resources/static/precache-manifest.eaaa37c58b6bf3046cfd14da7d6a39c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02b5fc57ba9a763b74f853dee787c9be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf93f9b8de8b4434c5f7",
+    "url": "/static/css/main.abbefc3a.chunk.css"
+  },
+  {
+    "revision": "b60162da186fa05cfe49",
+    "url": "/static/js/2.42f0a033.chunk.js"
+  },
+  {
+    "revision": "bf93f9b8de8b4434c5f7",
+    "url": "/static/js/main.6983d6df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
