805aa029aefddd545dcd02dc17ff9f3a26909a70
daeunnpark <daeunn@gmail.com>
2019-08-04 15:16:05 -0400

    file structure changed - doocke-compose.yml to root

---
+++ server/build/resources/main/static/precache-manifest.af917de0a253f907d8832d858ca8f7a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e9e438e42f9d8d6e14151921ba6d18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df2a1f83e31e5bc8e626",
+    "url": "/static/css/main.abbefc3a.chunk.css"
+  },
+  {
+    "revision": "b60162da186fa05cfe49",
+    "url": "/static/js/2.42f0a033.chunk.js"
+  },
+  {
+    "revision": "df2a1f83e31e5bc8e626",
+    "url": "/static/js/main.8d61e36c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ server/src/main/resources/static/precache-manifest.3f8324bfaa00e7f3dedb9c8d73c8d2e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a3b19152eb1f9c5193d474e72e76a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38927a882909036b5aa1",
+    "url": "/static/css/main.abbefc3a.chunk.css"
+  },
+  {
+    "revision": "b60162da186fa05cfe49",
+    "url": "/static/js/2.42f0a033.chunk.js"
+  },
+  {
+    "revision": "38927a882909036b5aa1",
+    "url": "/static/js/main.6586641b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
